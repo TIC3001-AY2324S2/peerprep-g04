e73d858935634ef2563ca802f9f68ef2bba594df
Kwok Cheong <wongkwokcheong@hotmail.com>
2024-02-26 15:51:20 +0800

    Initialize Question Service

--- question-service/server.js
+++ question-service/server.js
@@ -2,10 +2,10 @@ import http from "http";
 import index from "./index.js";
 import "dotenv/config.js";
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 const server = http.createServer(index);
 
-console.log("User Service - Starting on Port:", port);
+console.log("Question Service - Starting on Port: ", port);
 
 server.listen(port);
