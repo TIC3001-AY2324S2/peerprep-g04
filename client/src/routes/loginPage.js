6f470c2699bd5ba58f820e14d847ed816f040982
Kwok Cheong <wongkwokcheong@hotmail.com>
2024-03-03 03:42:42 +0800

    refactor login form

--- client/src/routes/loginPage.js
+++ client/src/routes/loginPage.js
@@ -2,10 +2,26 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
 import { useLoginUser } from "../hooks/api/user/useLoginUser";
 
 export default function LoginPage() {
+  // ----------------------------------
+  // FORM VALIDATIONS
+  // ----------------------------------
+  const schema = z.object({
+    email: z
+      .string()
+      .min(1, "Email is required")
+      .email({ message: "Invalid email address" }),
+    password: z
+      .string()
+      .min(1, "Password is required")
+      .min(8, { message: "Password must have at least 8 characters" }),
+  });
+
   // ----------------------------------
   // FORM SETUP
   // ----------------------------------
@@ -15,27 +31,8 @@ export default function LoginPage() {
     formState: { errors },
   } = useForm({
     mode: "onSubmit",
+    resolver: zodResolver(schema),
   });
-
-  // ----------------------------------
-  // FORM VALIDATIONS
-  // ----------------------------------
-  const validation = {
-    email: {
-      required: "Email is required",
-      pattern: {
-        value: /^[^@ ]+@[^@ ]+\.[^@ ]+$/,
-        message: "Invalid email address",
-      },
-    },
-    password: {
-      required: "Password is required",
-      minLength: {
-        value: 8,
-        message: "Password must have at least 8 characters",
-      },
-    },
-  };
 
   // ----------------------------------
   // TODO: ON SUBMIT HOOK
@@ -47,83 +44,11 @@ export default function LoginPage() {
   };
 
   return (
-    // <Card>
-    //   <form onSubmit={handleSubmit(onSubmit)} noValidate className="max-w-lg">
-    //     <h1 className="mb-4 font-bold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-2xl dark:text-white">
-    //       Sign in to your account
-    //     </h1>
-    //     Join our community of designers and developers to get access to hundreds
-    //     of UI components, plugins, resources, and design systems.
-    //     <br />
-    //     <br />
-    //     <div className="mb-5">
-    //       <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
-    //         Your email
-    //       </label>
-    //       <input
-    //         type="email"
-    //         name="email"
-    //         className={`block w-full rounded-lg p-2.5 text-sm text-gray-900 bg-gray-50
-    //                     ${
-    //                       errors.email
-    //                         ? "border-red-400 focus:border-red-500 focus:ring-red-500"
-    //                         : "border-gray-300 focus:border-blue-500 focus:ring-blue-500"
-    //                     }`}
-    //         placeholder="johndoe@gmail.com"
-    //         {...register("email", validation.email)}
-    //       />
-    //       <small className="mt-2 text-sm text-red-600 dark:text-red-500">
-    //         {errors?.email && errors.email.message}
-    //       </small>
-    //     </div>
-    //     <div className="mb-5">
-    //       <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
-    //         Your password
-    //       </label>
-    //       <input
-    //         type="password"
-    //         name="password"
-    //         autoComplete="current-password"
-    //         placeholder="password"
-    //         className={`block w-full rounded-lg p-2.5 text-sm text-gray-900 bg-gray-50
-    //                     ${
-    //                       errors.password
-    //                         ? "border-red-400 focus:border-red-500 focus:ring-red-500"
-    //                         : "border-gray-300 focus:border-blue-500 focus:ring-blue-500"
-    //                     }`}
-    //         {...register("password", validation.password)}
-    //       />
-    //       <small className="mt-2 text-sm text-red-600 dark:text-red-500">
-    //         {errors?.password && errors.password.message}
-    //       </small>
-    //     </div>
-    //     <div className="mb-5 flex items-start">
-    //       <div className="flex h-5 items-center">
-    //         <input
-    //           id="remember"
-    //           type="checkbox"
-    //           className="focus:ring-3 h-4 w-4 rounded border border-gray-300 bg-gray-50 focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800"
-    //           {...register("remember")}
-    //           required
-    //         />
-    //       </div>
-    //       <label
-    //         htmlFor="remember"
-    //         className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-    //       >
-    //         Remember me
-    //       </label>
-    //     </div>
-    //     <div>
-    //       <Button type="submit">Submit</Button>
-    //     </div>
-    //   </form>
-    // </Card>
-    <Card>
+    <Card className="max-w-3xl">
       <form
         onSubmit={handleSubmit(onSubmit)}
         noValidate
-        className="flex flex-col"
+        className="flex flex-col gap-4"
       >
         <h1 className="mb-4 font-bold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-2xl dark:text-white">
           Sign in to your account
@@ -141,7 +66,8 @@ export default function LoginPage() {
             type="email"
             placeholder="name@flowbite.com"
             required
-            {...register("email", validation.email)}
+            color={`${errors.email ? "failure" : "gray"}`}
+            {...register("email")}
           />
           <small className="mt-2 text-sm text-red-600 dark:text-red-500">
             {errors?.email && errors.email.message}
@@ -155,7 +81,8 @@ export default function LoginPage() {
             id="password1"
             type="password"
             required
-            {...register("password", validation.password)}
+            color={`${errors.password ? "failure" : "gray"}`}
+            {...register("password")}
           />
           <small className="mt-2 text-sm text-red-600 dark:text-red-500">
             {errors?.password && errors.password.message}
