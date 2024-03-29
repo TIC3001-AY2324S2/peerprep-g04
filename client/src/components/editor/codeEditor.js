445a44db6ec1576fdf639b64f8c90833fc5d4d96
Kwok Cheong <wongkwokcheong@hotmail.com>
2024-03-29 21:40:50 +0800

    feat(client): update question description page

--- client/src/components/editor/codeEditor.js
+++ client/src/components/editor/codeEditor.js
@@ -45,6 +45,7 @@ const CodeEditor = () => {
   return (
     <div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl">
       <CodeMirror
+        style={{ fontSize: "12px" }}
         value={userCode}
         height="50vh"
         theme={vscodeDark}
--- client/src/routes/questionDetailsPage.js
+++ client/src/routes/questionDetailsPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useGetQuestionById } from "../hooks/api/question/useGetQuestionById";
 import { useParams } from "react-router-dom";
-import { Card } from "flowbite-react";
 import CodeEditor from "../components/editor/codeEditor";
 
 export default function QuestionDetailsPage() {
@@ -11,23 +10,23 @@ export default function QuestionDetailsPage() {
   });
 
   return (
-    <div className="w-full h-full">
-      <div className="flex flex-row p-8">
+    <div className="w-full h-full" style={{ width: "90%" }}>
+      <div className="flex flex-row p-8 h-full">
         {!isLoading ? (
-          <div className="w-1/2 min-w-xl m-2">
-            <Card>
+          <div className="w-1/2 m-2">
+            <section className="h-full block p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 {data.data.title}
               </h5>
               <p className="font-normal text-gray-700 dark:text-gray-400">
                 {data.data.description}
               </p>
-            </Card>
+            </section>
           </div>
         ) : (
           <div>Loading...</div>
         )}
-        <div className="w-1/2 min-w-xl m-2" style={{ height: "500px" }}>
+        <div className="w-1/2 m-2">
           <CodeEditor />
         </div>
       </div>
