da114f42d7a3f74f280e2b9b76d46e468e017841
Kwok Cheong <wongkwokcheong@hotmail.com>
2024-04-14 11:28:25 +0700

    feat(web): Qingwen - update question detail page

--- client/src/routes/questionDetailsPage.js
+++ client/src/routes/questionDetailsPage.js
@@ -13,23 +13,30 @@ export default function QuestionDetailsPage() {
   const [markdownVisible, setMarkdownVisible] = useState(true);
 
   const toggleMakeCodeEditorEnlarged = () => {
-    const firstDiv = document.getElementById('first');
-    const secondDiv = document.getElementById('second');
+    const firstDiv = document.getElementById("first");
+    const secondDiv = document.getElementById("second");
 
     setMarkdownVisible(!markdownVisible);
 
-    firstDiv.classList.toggle('w-1/2');
-    firstDiv.classList.toggle('w-1/20');
+    firstDiv.classList.toggle("w-1/2");
+    firstDiv.classList.toggle("w-1/20");
 
-    secondDiv.classList.toggle('w-1/2');
-    secondDiv.classList.toggle('w-full');
+    secondDiv.classList.toggle("w-1/2");
+    secondDiv.classList.toggle("w-full");
   };
 
   // Function to split description based on keyword
   const splitDescription = (description) => {
-    const keywords = ['input:', 'e.g.', 'E.g.', 'Input:', 'Example:', 'example:'];
+    const keywords = [
+      "input:",
+      "e.g.",
+      "E.g.",
+      "Input:",
+      "Example:",
+      "example:",
+    ];
     let index = -1;
-    let keywordFound = '';
+    let keywordFound = "";
 
     // Find the index of the first keyword occurring in the description
     for (const kw of keywords) {
@@ -43,23 +50,25 @@ export default function QuestionDetailsPage() {
     if (index !== -1) {
       // Extracting the first and second halves based on the found keyword
       let firstHalf = description.substring(0, index).trim();
-      let secondHalf = description.substring(index + keywordFound.length).trim();
+      let secondHalf = description
+        .substring(index + keywordFound.length)
+        .trim();
       return [firstHalf, secondHalf];
     }
 
     // If no keyword found, return original description
-    return [description, ''];
+    return [description, ""];
   };
 
   // Render the description
   const renderDescription = (description) => {
     let [firstHalf, secondHalf] = splitDescription(description);
-    if (firstHalf.endsWith(',')) {
+    if (firstHalf.endsWith(",")) {
       firstHalf = firstHalf.slice(0, -1);
-      firstHalf = firstHalf + '.';
+      firstHalf = firstHalf + ".";
     }
 
-    const answerKeywords = ['output:', 'Output:', 'Answer:', 'answer:'];
+    const answerKeywords = ["output:", "Output:", "Answer:", "answer:"];
 
     let answerKeywordFound = false;
     let indexOfAnsKw;
@@ -82,17 +91,25 @@ export default function QuestionDetailsPage() {
       secondHalf = secondHalf.substring(0, indexOfAnsKw).trim();
     }
 
-    console.log("First Half:", firstHalf);
-    console.log("Second Half:", secondHalf);
-    console.log("Third Half:", thirdHalf);
-
     return (
       <div>
         <p>{firstHalf}</p>
         <br></br>
-        {secondHalf && <p><strong>Input: </strong><br></br>{secondHalf}</p>}
+        {secondHalf && (
+          <p>
+            <strong>Input: </strong>
+            <br></br>
+            {secondHalf}
+          </p>
+        )}
         <br></br>
-        {thirdHalf && <p><strong>Output: </strong><br></br>{thirdHalf}</p>}
+        {thirdHalf && (
+          <p>
+            <strong>Output: </strong>
+            <br></br>
+            {thirdHalf}
+          </p>
+        )}
       </div>
     );
   };
@@ -103,14 +120,25 @@ export default function QuestionDetailsPage() {
         {!isLoading ? (
           <div className="w-1/2 m-2" id="first">
             <section className="h-full block p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <span className="flex justify-end cursor-pointer" onClick={toggleMakeCodeEditorEnlarged}>&lt;</span>
-              {markdownVisible && <h5 className="text-2xl tracking-tight text-gray-900 dark:text-white flex justify-center">
-                Problem
-              </h5>}
-              {markdownVisible && <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white flex justify-center">
-                {data.data.title}
-              </h5>}
-              {markdownVisible && <hr className="my-2 border-gray-300 dark:border-gray-700" />}
+              <span
+                className="flex justify-end cursor-pointer"
+                onClick={toggleMakeCodeEditorEnlarged}
+              >
+                &lt;
+              </span>
+              {markdownVisible && (
+                <h5 className="text-2xl tracking-tight text-gray-900 dark:text-white flex justify-center">
+                  Problem
+                </h5>
+              )}
+              {markdownVisible && (
+                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white flex justify-center">
+                  {data.data.title}
+                </h5>
+              )}
+              {markdownVisible && (
+                <hr className="my-2 border-gray-300 dark:border-gray-700" />
+              )}
               {markdownVisible && renderDescription(data.data.description)}
             </section>
           </div>
