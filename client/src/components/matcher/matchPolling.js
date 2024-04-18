eb1ead3607e508218c932063223c2e745429e56f
Kwok Cheong <wongkwokcheong@hotmail.com>
2024-04-18 20:44:22 +0800

    refactor(client): remove polling and use interval to fetch instead

--- client/src/components/matcher/matchPolling.js
+++ client/src/components/matcher/matchPolling.js
@@ -1,34 +1,34 @@
 import { useEffect } from "react";
 import { Button } from "flowbite-react";
-import { useGetPollMatchByUserId } from "../../hooks/api/match/useGetPollMatchByUserId";
 import { useNavigate } from "react-router-dom";
 import { RoundLoading } from "../common/roundLoading";
 import { useJoinQueue } from "../../hooks/api/match/useJoinQueue";
+import { useGetFindMatchByUserId } from "../../hooks/api/match/useGetFindMatchByUserId";
 
 export const MatchPolling = ({ userId, formData }) => {
   const navigate = useNavigate();
-  const {
-    data: foundMatch,
-    isLoading,
-    setShouldFetch,
-  } = useGetPollMatchByUserId(userId.userId);
-
   const { mutate: joinQueue } = useJoinQueue();
+
+  const {
+    data: isMatched,
+    isLoading: isLoadingFindMatched,
+    refetch,
+  } = useGetFindMatchByUserId(userId);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      refetch();
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [refetch]);
 
   const onSubmit = () => {
     const { matchType, ...restFormData } = formData;
     joinQueue({ data: { matchType: "ANY", ...restFormData } });
   };
-
-  useEffect(() => {
-    // Start the polling when the component is mounted
-    if (foundMatch) {
-      setShouldFetch(false);
-      navigate("/matchDetails");
-    } else {
-      setShouldFetch(true);
-    }
-  }, [foundMatch]); // Empty dependency array means this effect runs once on mount
 
   return (
     <div>
